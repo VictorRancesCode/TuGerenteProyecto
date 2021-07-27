2a231b48c621ef0b2a3b2ee429ac3f719a43a11b
Victor Alfonso Rodas Oña <victordevcode@gmail.com>
2021-07-27 13:20:00 -0400

    fix static

--- project/settings/base.py
+++ project/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -123,3 +124,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Django Money
 CURRENCIES = ('USD', 'EUR', 'BOB')
 CURRENCY_CHOICES = [('USD', 'USD $'), ('EUR', 'EUR €'), ('BOB', 'BOB Bs')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- project/settings/production.py
+++ project/settings/production.py
@@ -3,15 +3,8 @@ from .base import *
 import dj_database_url
 
 DEBUG = False
-
-
-
-
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ALLOWED_HOSTS = ['https://tu-gerente-booking.herokuapp.com']
-
-
-
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.setdefault('DATABASE_URL', '')
