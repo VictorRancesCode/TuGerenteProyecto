63c2d41e7cf052f0d1a3ceb7dcb1d9ba94affb31
Victor Alfonso Rodas Oña <victordevcode@gmail.com>
2021-07-27 13:38:25 -0400

    update ALLOWED_HOSTS

--- project/settings/production.py
+++ project/settings/production.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 DEBUG = False
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-ALLOWED_HOSTS = ['https://tu-gerente-booking.herokuapp.com']
+ALLOWED_HOSTS = ['tu-gerente-booking.herokuapp.com','.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.setdefault('DATABASE_URL', '')
