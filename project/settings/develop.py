5fabe79aefe737a5d4f0f4223e975884f498389f
Victor Alfonso Rodas Oña <victordevcode@gmail.com>
2021-07-27 12:32:06 -0400

    config

--- project/settings/develop.py
+++ project/settings/develop.py
@@ -2,4 +2,11 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
